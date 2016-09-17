aa1e3201bcc10c8eab027fbf1a6f7fddab27afcb
buggy213 <awesomejosh1337@gmail.com>
2016-09-17 14:28:02 -0700

    Initial commit

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HardwareK9bot.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HardwareK9bot.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 /**
  * This is NOT an opmode.
  *
